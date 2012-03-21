7ef11d71d45a0348cab741df38929ddd82b44993
TaylorE <taylor.eichhorst@gmail.com>
2012-03-21 15:28:26 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaylorTestapp::Application.config.secret_token = '2ae6055d5ec4c73529b4aceb6ea9e10ce7e702d9e234bd90e0dddd0808bd90acb2d1717531b51024deb25dc7219bbed3d446e54502d72cb5bb46b13781ece358'
